59775592815801df01e9f5fd72b4443a351531d3
Maxim Neverov <neverov.max@gmail.com>
2018-12-04 20:06:56 +0100

    Added simple mean calculation & endpoint to retrieve

--- sample/src/main/scala/mn/SampleAkkaHttpServer.scala
+++ sample/src/main/scala/mn/SampleAkkaHttpServer.scala
@@ -1,5 +1,7 @@
 package mn
 
+import java.util.concurrent.atomic.AtomicLong
+
 import akka.Done
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
@@ -20,18 +22,43 @@ object SampleAkkaHttpServer {
 
   private val db = new java.util.concurrent.ConcurrentHashMap[String, User]()
 
+  private var saveAcc = new AtomicLong(0)
+  private var saveCnt = new AtomicLong(0)
+
+  private var getAcc = new AtomicLong(0)
+  private var getCnt = new AtomicLong(0)
+
   def saveUser(user: User): Future[Either[String, Done]] = {
+    val start = System.nanoTime()
     try {
       db.putIfAbsent(user.name, user)
       Future.successful(Right(Done))
     } catch {
       case NonFatal(e) => Future.successful(Left(e.getMessage))
+    } finally {
+      saveCnt.incrementAndGet()
+      saveAcc.addAndGet(System.nanoTime() - start)
     }
   }
 
   def getUser(name: String): Future[Option[User]] = {
-    val user = db.get(name)
-    if (user == null) Future.successful(None) else Future.successful(Some(user))
+    val start = System.nanoTime()
+
+    try {
+      val user = db.get(name)
+      if (user == null) Future.successful(None) else Future.successful(Some(user))
+    } finally {
+      getCnt.incrementAndGet()
+      getAcc.addAndGet(System.nanoTime() - start)
+    }
+  }
+
+  def printTimes(): String = {
+    var res = s"Save median (${saveAcc.get}; ${saveCnt.get}): " + (1.0 * saveAcc.get() / saveCnt.get) / 1000000
+    res += s"\n Get median (${getAcc.get}; ${getCnt.get}): " + (1.0 * getAcc.get() / getCnt.get) / 1000000
+
+    println(res)
+    res
   }
 
   def main(args: Array[String]): Unit = {
@@ -42,33 +69,42 @@ object SampleAkkaHttpServer {
     implicit val executionContext = system.dispatcher
 
 
-    val route = pathPrefix("user") {
-      concat(
-        get {
-          path(Segment) { name =>
-            pathEndOrSingleSlash {
-              onComplete(getUser(name)) {
-                case Success(None) => complete(StatusCodes.NotFound, ResponseError(123, s"User $name not found"))
-                case Success(Some(user)) => complete(user)
+    val route = concat(
+      pathPrefix("user") {
+        concat(
+          get {
+            path(Segment) { name =>
+              pathEndOrSingleSlash {
+                onComplete(getUser(name)) {
+                  case Success(None) => complete(StatusCodes.NotFound, ResponseError(123, s"User $name not found"))
+                  case Success(Some(user)) => complete(user)
+                  case Failure(e) => complete(StatusCodes.InternalServerError,
+                    ResponseError(234, s"Failed to retrieve user [$name]: ${e.getMessage}"))
+                }
+              }
+            }
+          },
+          post {
+            entity(as[User]) { user =>
+              onComplete(saveUser(user)) {
+                case Success(Left(msg)) => complete(StatusCodes.InternalServerError,
+                  ResponseError(234, s"Failed to save user: $user"))
+                case Success(Right(_)) => complete(StatusCodes.Created)
                 case Failure(e) => complete(StatusCodes.InternalServerError,
-                  ResponseError(234, s"Failed to retrieve user [$name]: ${e.getMessage}"))
+                  ResponseError(234, s"Failed to save user $user: ${e.getMessage}"))
               }
             }
           }
-        },
-        post {
-          entity(as[User]) { user =>
-            onComplete(saveUser(user)) {
-              case Success(Left(msg)) => complete(StatusCodes.InternalServerError,
-                ResponseError(234, s"Failed to save user: $user"))
-              case Success(Right(_)) => complete(StatusCodes.Created)
-              case Failure(e) => complete(StatusCodes.InternalServerError,
-                ResponseError(234, s"Failed to save user $user: ${e.getMessage}"))
-            }
+        )
+      },
+      pathPrefix("stat") {
+        get {
+          pathEndOrSingleSlash {
+            complete(printTimes())
           }
         }
-      )
-    }
+      }
+    )
 
     val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 8080)
 
