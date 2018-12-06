a2062881b4a813c8f4218f8fa84936d741720fc5
Maxim Neverov <neverov.max@gmail.com>
2018-12-06 18:54:21 +0100

    Implemented db populating before main app starts

--- sample/db/init_db.sql
+++ sample/db/init_db.sql
@@ -2,3 +2,12 @@ CREATE TABLE users (
   name     VARCHAR(200) CONSTRAINT user_pk PRIMARY KEY,
   whatever int          NOT NULL
 );
+
+DO $$
+BEGIN
+  FOR i IN 1..100000 LOOP
+    insert into users(name, whatever)
+    values((SELECT MD5(random()::text) || i), i);
+  END LOOP;
+END;
+$$ LANGUAGE plpgsql;
